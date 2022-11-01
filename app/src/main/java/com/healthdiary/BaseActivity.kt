a141605a4577a824d471db5d522f2ab99d101be9
Distories <18663977305@163.com>
2022-11-02 00:13:38 +0800

    Fix: Hide Status Bar
    Using comments method will cause the app to crash, and now don't know why, so I'm going to use another method, which is deprecated, but it works.

--- app/src/main/java/com/healthdiary/BaseActivity.kt
+++ app/src/main/java/com/healthdiary/BaseActivity.kt
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.doOnLayout
 import kotlin.system.exitProcess
@@ -17,17 +18,31 @@ open class BaseActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
     }
+/*    Using comments method will cause the app to crash, and now don't know why,
+      so I'm going to use the method below, which is deprecated, but it works.
+ */
+//    open fun hideStatusAndActionBar() {
+//        val decorView = window.decorView
+//        // Hide the status bar.
+//        decorView.doOnLayout {
+//            decorView.windowInsetsController?.hide(android.view.WindowInsets.Type.statusBars())
+//        }
+//
+//        // Hide the action bar.
+//        supportActionBar!!.hide()
+//    }
+
 
     open fun hideStatusAndActionBar() {
-        val decorView = window.decorView
-        // Hide the status bar.
-        decorView.doOnLayout {
-            decorView.windowInsetsController?.hide(android.view.WindowInsets.Type.statusBars())
-        }
+    val decorView = window.decorView
+    // Hide the status bar.
+    val uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN
+    decorView.systemUiVisibility = uiOptions
 
-        // Hide the action bar.
-        supportActionBar!!.hide()
+    // Hide the action bar.
+    supportActionBar!!.hide()
     }
+
     open fun showStatusAndActionBar() {
         val decorView = window.decorView
         // Hide the status bar.
