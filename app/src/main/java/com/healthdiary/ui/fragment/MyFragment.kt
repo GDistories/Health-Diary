cacac14ef976a3f291f3c849b5bf90262af0bd6b
Distories <18663977305@163.com>
2022-11-03 07:43:18 +0800

    Using binding in  my fragment

--- app/src/main/java/com/healthdiary/ui/fragment/MyFragment.kt
+++ app/src/main/java/com/healthdiary/ui/fragment/MyFragment.kt
@@ -6,23 +6,32 @@ import android.view.View
 import android.view.ViewGroup
 import com.healthdiary.base.BaseFragment
 import com.healthdiary.R
+import com.healthdiary.databinding.FragmentMyBinding
 
 
 class MyFragment : BaseFragment() {
+    private var _binding: FragmentMyBinding? = null
+
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentMyBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_my, container, false)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
