35061647c88dd2d343bef409f05200c395e14ac8
Distories <18663977305@163.com>
2022-11-03 08:54:27 +0800

    Add about apge

--- app/src/main/java/com/healthdiary/ui/fragment/MyFragment.kt
+++ app/src/main/java/com/healthdiary/ui/fragment/MyFragment.kt
@@ -1,12 +1,13 @@
 package com.healthdiary.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.healthdiary.base.BaseFragment
-import com.healthdiary.R
 import com.healthdiary.databinding.FragmentMyBinding
+import com.healthdiary.ui.activity.AboutActivity
 
 
 class MyFragment : BaseFragment() {
@@ -18,16 +19,19 @@ class MyFragment : BaseFragment() {
         super.onCreate(savedInstanceState)
     }
 
+    override fun onStart() {
+        super.onStart()
+        binding.about.setOnClickListener { startActivity(Intent(activity, AboutActivity::class.java)) }
+    }
 
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentMyBinding.inflate(inflater, container, false)
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my, container, false)
+        return binding.root
     }
 
     override fun onDestroyView() {
