58d583ebc5826f5505d5f2d7974a1e044725109c
Distories <18663977305@163.com>
2022-11-03 15:22:12 +0800

    Add Check for Update

--- app/src/main/java/com/healthdiary/ui/fragment/MyFragment.kt
+++ app/src/main/java/com/healthdiary/ui/fragment/MyFragment.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.healthdiary.R
 import com.healthdiary.base.BaseFragment
 import com.healthdiary.databinding.FragmentMyBinding
 import com.healthdiary.ui.activity.AboutActivity
@@ -21,6 +23,10 @@ class MyFragment : BaseFragment() {
 
     override fun onStart() {
         super.onStart()
+        binding.update.setOnClickListener {
+            Toast.makeText(activity, R.string.searching_update, Toast.LENGTH_SHORT).show()
+            Toast.makeText(activity, R.string.no_update, Toast.LENGTH_SHORT).show()
+        }
         binding.about.setOnClickListener { startActivity(Intent(activity, AboutActivity::class.java)) }
     }
 
