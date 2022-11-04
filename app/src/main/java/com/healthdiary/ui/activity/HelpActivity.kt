727d0fffb36b735bfeb4fdadf8341ae43f7055c1
Distories <18663977305@163.com>
2022-11-04 12:20:35 +0800

    Add Search

--- app/src/main/java/com/healthdiary/ui/activity/HelpActivity.kt
+++ app/src/main/java/com/healthdiary/ui/activity/HelpActivity.kt
@@ -3,10 +3,12 @@ package com.healthdiary.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.healthdiary.R
+import com.healthdiary.base.BaseActivity
 
-class HelpActivity : AppCompatActivity() {
+class HelpActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help)
+        hideStatusAndActionBar()
     }
 }
