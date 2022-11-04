0ebee066a7cc27a876960ea0192bed56b31208a4
Distories <18663977305@163.com>
2022-11-04 16:31:30 +0800

    Finish Reset and register activity

---
+++ app/src/main/java/com/healthdiary/ui/activity/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.healthdiary.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.healthdiary.R
+import com.healthdiary.base.BaseActivity
+
+class RegisterActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
