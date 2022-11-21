5d278abf5ed74b23f815171f53b794998449a969
yolol02 <1027515604@qq.com>
2022-11-21 17:21:55 +0800

    Doctor detail page UI finish

--- app/src/main/java/com/healthdiary/ui/activity/DoctorInfoActivity.kt
+++ app/src/main/java/com/healthdiary/ui/activity/DoctorInfoActivity.kt
@@ -3,10 +3,22 @@ package com.healthdiary.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.healthdiary.R
+import com.healthdiary.base.BaseActivity
+import com.healthdiary.databinding.ActivityDoctorInfoBinding
 
-class DoctorInfoActivity : AppCompatActivity() {
+
+class DoctorInfoActivity : BaseActivity() {
+    private lateinit var binding: ActivityDoctorInfoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_doctor_info)
+        binding = ActivityDoctorInfoBinding.inflate(layoutInflater)
+        hideStatusAndActionBar()
+        val view = binding.root
+        setContentView(view)
+
+        binding.ivBack.setOnClickListener {
+            finish()
+        }
     }
 }
