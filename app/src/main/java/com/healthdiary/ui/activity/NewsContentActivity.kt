bd202ae2652ae6d0b78bfff5ae979eba270d1492
Distories <18663977305@163.com>
2022-11-25 05:02:49 +0800

    Fix: News Content Bug By Changing New Video Player

--- app/src/main/java/com/healthdiary/ui/activity/NewsContentActivity.kt
+++ app/src/main/java/com/healthdiary/ui/activity/NewsContentActivity.kt
@@ -1,14 +1,10 @@
 package com.healthdiary.ui.activity
 
-
-import android.net.Uri
-import android.widget.MediaController
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import cn.jzvd.Jzvd
 import com.healthdiary.R
 import com.healthdiary.base.BaseActivity
 import com.healthdiary.databinding.ActivityNewsContentBinding
-import kotlinx.android.synthetic.main.activity_news_content.*
 
 class NewsContentActivity : BaseActivity() {
     private lateinit var binding: ActivityNewsContentBinding
@@ -18,18 +14,20 @@ class NewsContentActivity : BaseActivity() {
         setContentView(binding.root)
         hideStatusAndActionBar()
 
-        val mediaController = MediaController(this)
-        mediaController.setAnchorView(videoView)
+        val onlineUrl = "https://www.shutterstock.com/shutterstock/videos/1060058060/preview/stock-footage-asia-businessmen-and-businesswomen-meeting-brainstorming-ideas-about-new-paperwork-colleagues.webm"
 
-        val onlineUri = Uri.parse("https://www.shutterstock.com/shutterstock/videos/1060058060/preview/stock-footage-asia-businessmen-and-businesswomen-meeting-brainstorming-ideas-about-new-paperwork-colleagues.webm")
-
-        videoView.setMediaController(mediaController)
-        videoView.setVideoURI(onlineUri)
-        videoView.requestFocus()
-        videoView.start()
+        //初始化视频播放器的视频链接和标题
+        binding.jzvdVideo.setUp(onlineUrl, "视频标题")
+        //设置视频播放器的封面，视频默认是暂停状态
+        binding.jzvdVideo.posterImageView.setImageResource(R.drawable.image_simple_watch)
 
         binding.ivBack.setOnClickListener {
             finish()
         }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Jzvd.releaseAllVideos()
     }
 }
