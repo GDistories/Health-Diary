ff21bdd0d9302284a5d8063578b0fe318726e050
yolol02 <1027515604@qq.com>
2022-11-26 19:34:40 +0800

    Minor modification

--- app/src/main/java/com/healthdiary/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/healthdiary/ui/fragment/HomeFragment.kt
@@ -1,21 +1,31 @@
 package com.healthdiary.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.healthdiary.base.BaseFragment
 import com.healthdiary.R
+import com.healthdiary.databinding.FragmentHomeBinding
+import com.healthdiary.ui.activity.NewsContentActivity
 
 
 class HomeFragment : BaseFragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private var _binding: FragmentHomeBinding? = null
+
+    private val binding get() = _binding!!
+            override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
+    override fun onStart() {
+        super.onStart()
+        binding.news1.setOnClickListener {
+            startActivity(Intent(activity, NewsContentActivity::class.java))
+        }
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,7 +33,11 @@ class HomeFragment : BaseFragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding =FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
-
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
