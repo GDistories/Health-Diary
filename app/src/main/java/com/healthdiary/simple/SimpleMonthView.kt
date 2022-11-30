314b4e0aa6dea000335f3f08311edb513d628f98
Distories <18663977305@163.com>
2022-11-30 20:46:17 +0800

    Finish Calendar in health fragment

---
+++ app/src/main/java/com/healthdiary/simple/SimpleMonthView.kt
@@ -0,0 +1,68 @@
+package com.healthdiary.simple
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import com.haibin.calendarview.Calendar
+import com.haibin.calendarview.MonthView
+
+class SimpleMonthView(context: Context?) : MonthView(context) {
+    private var mRadius = 0
+    override fun onPreviewHook() {
+        mRadius = Math.min(mItemWidth, mItemHeight) / 5 * 2
+        mSchemePaint.style = Paint.Style.STROKE
+        mSchemePaint.strokeWidth = (mItemWidth / 10.0).toFloat()
+    }
+
+    override fun onLoopStart(x: Int, y: Int) {}
+    override fun onDrawSelected(
+        canvas: Canvas,
+        calendar: Calendar,
+        x: Int,
+        y: Int,
+        hasScheme: Boolean
+    ): Boolean {
+        val cx = x + mItemWidth / 2
+        val cy = y + mItemHeight / 2
+        canvas.drawCircle(cx.toFloat(), cy.toFloat(), mRadius.toFloat(), mSelectedPaint)
+        return false
+    }
+
+    override fun onDrawScheme(canvas: Canvas, calendar: Calendar, x: Int, y: Int) {
+        val cx = x + mItemWidth / 2
+        val cy = y + mItemHeight / 2
+        canvas.drawCircle(cx.toFloat(), cy.toFloat(), mRadius.toFloat(), mSchemePaint)
+    }
+
+    override fun onDrawText(
+        canvas: Canvas,
+        calendar: Calendar,
+        x: Int,
+        y: Int,
+        hasScheme: Boolean,
+        isSelected: Boolean
+    ) {
+        val baselineY = mTextBaseLine + y
+        val cx = x + mItemWidth / 2
+        if (isSelected) {
+            canvas.drawText(
+                calendar.day.toString(),
+                cx.toFloat(),
+                baselineY,
+                mSelectTextPaint
+            )
+        } else if (hasScheme) {
+            canvas.drawText(
+                calendar.day.toString(),
+                cx.toFloat(),
+                baselineY,
+                if (calendar.isCurrentDay) mCurDayTextPaint else if (calendar.isCurrentMonth) mSchemeTextPaint else mOtherMonthTextPaint
+            )
+        } else {
+            canvas.drawText(
+                calendar.day.toString(), cx.toFloat(), baselineY,
+                if (calendar.isCurrentDay) mCurDayTextPaint else if (calendar.isCurrentMonth) mCurMonthTextPaint else mOtherMonthTextPaint
+            )
+        }
+    }
+}
--- app/src/main/java/com/healthdiary/ui/fragment/HealthFragment.kt
+++ app/src/main/java/com/healthdiary/ui/fragment/HealthFragment.kt
@@ -1,28 +1,162 @@
 package com.healthdiary.ui.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.healthdiary.base.BaseFragment
+import com.haibin.calendarview.Calendar
+import com.haibin.calendarview.CalendarView
 import com.healthdiary.R
+import com.healthdiary.base.BaseFragment
+import com.healthdiary.databinding.FragmentHealthBinding
+import com.healthdiary.databinding.FragmentMyBinding
+import java.text.DateFormat
+import java.text.DecimalFormat
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.abs
 
 
-class HealthFragment : BaseFragment() {
+open class HealthFragment : BaseFragment(), CalendarView.OnCalendarSelectListener, CalendarView.OnYearChangeListener,
+    View.OnClickListener {
+
+    private var _binding: FragmentHealthBinding? = null
+    private var mYear: Int = 0
+    private val binding get() = _binding!!
+    private var green = -0x66bf24db
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
+    override fun onStart() {
+        super.onStart()
+        initView()
+        initData()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
+        _binding = null
     }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_health, container, false)
+    ): View {
+        _binding = FragmentHealthBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+
+    private fun initView() {
+        binding.tvMonthDay.setOnClickListener(View.OnClickListener {
+            if (!binding.calendarLayout.isExpand) {
+                binding.calendarLayout.expand()
+                return@OnClickListener
+            }
+            binding.calendarView.showYearSelectLayout(mYear)
+            binding.tvLunar.visibility = View.GONE
+            binding.tvYear.visibility = View.GONE
+            binding.tvMonthDay.text = mYear.toString()
+        })
+
+        binding.flCurrent.setOnClickListener {
+            binding.calendarView.scrollToCurrent()
+        }
+
+        binding.calendarView.setOnYearChangeListener(this)
+        binding.calendarView.setOnCalendarSelectListener(this)
+        binding.tvYear.text = binding.calendarView.curYear.toString()
+        mYear = binding.calendarView.curYear
+        val g1 = DecimalFormat("00")
+        binding.tvMonthDay.text = g1.format(binding.calendarView.curMonth.toLong()) + "-" + g1.format(
+            binding.calendarView.curDay.toLong()
+        )
+        binding.tvLunar.text = getString(R.string.today)
+        binding.tvCurrentDay.text = binding.calendarView.curDay.toString()
+    }
+
+    protected open fun initData() {
+        val year: Int = binding.calendarView.curYear
+        val month: Int = binding.calendarView.curMonth
+        val map: MutableMap<String, Calendar> = HashMap()
+        map[getSchemeCalendar(year, month, 3, green).toString()] =
+            getSchemeCalendar(year, month, 3, green)
+        map[getSchemeCalendar(year, month, 6, green).toString()] =
+            getSchemeCalendar(year, month, 6, green)
+        map[getSchemeCalendar(year, month, 9, green).toString()] =
+            getSchemeCalendar(year, month, 9, green)
+        map[getSchemeCalendar(year, month, 13, green).toString()] =
+            getSchemeCalendar(year, month, 13, green)
+        map[getSchemeCalendar(year, month, 14, green).toString()] =
+            getSchemeCalendar(year, month, 14, green)
+        map[getSchemeCalendar(year, month, 15, green).toString()] =
+            getSchemeCalendar(year, month, 15, green)
+        map[getSchemeCalendar(year, month, 18, green).toString()] =
+            getSchemeCalendar(year, month, 18, green)
+        map[getSchemeCalendar(year, month, 25, green).toString()] =
+            getSchemeCalendar(year, month, 25, green)
+        map[getSchemeCalendar(year, month, 27, green).toString()] =
+            getSchemeCalendar(year, month, 27, green)
+        binding.calendarView.setSchemeDate(map)
+    }
+
+    private fun getSchemeCalendar(
+        year: Int,
+        month: Int,
+        day: Int,
+        color: Int
+    ): Calendar {
+        val calendar = Calendar()
+        calendar.year = year
+        calendar.month = month
+        calendar.day = day
+        calendar.schemeColor = color
+        calendar.scheme = ""
+        return calendar
+    }
+    override fun onCalendarSelect(calendar: Calendar, isClick: Boolean) {
+        binding.tvLunar.visibility = View.VISIBLE
+        binding.tvYear.visibility = View.VISIBLE
+        val g1 = DecimalFormat("00")
+        binding.tvMonthDay.text = g1.format(calendar.month.toLong()) + "-" + g1.format(calendar.day.toLong())
+        binding.tvYear.text = calendar.year.toString()
+        binding.tvLunar.text = calendar.lunar
+        mYear = calendar.year
+        calculateDayDifference(calendar)
+    }
+
+    override fun onYearChange(year: Int) {
+        binding.tvMonthDay.text = year.toString()
+    }
+
+    open fun calculateDayDifference(calendar: Calendar) {
+        @SuppressLint("SimpleDateFormat") val df: DateFormat = SimpleDateFormat("yyyyMMdd")
+        try {
+            val d1 = df.parse(calendar.toString())
+            val today = df.parse(df.format(Date()))
+            if (d1 != null && today != null) {
+                val diff = (d1.time - today.time).toDouble()
+                val days = diff / (1000 * 60 * 60 * 24)
+                Log.e("TAG", days.toString())
+                if (days > 0) {
+                    binding.tvLunar.text = days.toInt().toString() + " " + getString(R.string.days_later)
+                } else if (days < 0) {
+                    binding.tvLunar.text = abs(days.toInt()).toString() + " " + getString(R.string.days_ago)
+                } else {
+                    binding.tvLunar.text = getString(R.string.today)
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun onClick(v: View?) {}
+    override fun onCalendarOutOfRange(calendar: Calendar?) {}
 }
+
